d04205f3c455dc2c641e52743b99fd1d6633ea62
aaronphilips <aphilips@ualberta.ca>
2017-02-28 19:54:34 -0700

    polish with other docs

--- FileWatcher.rb
+++ FileWatcher.rb
@@ -11,13 +11,13 @@ module FileWatcher
     list_filenames.each do |filename|
       file_name_hash[filename]= Dir.glob("/**/"+ filename).size
     end
-    # pre con
+
     fork {
       while(!file_name_hash.empty?)
         file_name_hash.each do |filename,count|
           if Dir.glob("/**/"+ filename).size>count
             timed_delayed_message(duration)
-            message("File __ been created")
+            message("File #{filename} been created")
             yield
             file_name_hash.delete(filename)
           end
@@ -32,18 +32,18 @@ module FileWatcher
 
   def alter(duration, list_filenames)
     pre_alter(duration, list_filenames)
-    # HAVE TO CHECK THAT FILES DON"T EXIST
+
     time_check=Time.new
-    # pre con
+
     fork {
       while(!list_filenames.empty?)
         list_filenames.each do |filename|
 
-          # if Dir.glob("/**/"+ filename).size>count
+
           Dir.glob("/**/"+ filename).each do |filepath|
             if File.mtime(filepath)>time_check
               timed_delayed_message(duration)
-              message("File __ been created")
+              message("File #{filename} been created")
               yield
               list_filenames.delete(filename)
             end
@@ -67,7 +67,7 @@ module FileWatcher
         file_name_hash.each do |filename,count|
           if Dir.glob("/**/"+ filename).size<count
             timed_delayed_message(duration)
-            message("File __ been created")
+            message("File #{filename} been created")
             yield
             file_name_hash.delete(filename)
           end
