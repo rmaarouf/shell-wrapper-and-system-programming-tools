64cdcf2d89c33c0381437208b08f6e95d42096d3
aaronphilips <aphilips@ualberta.ca>
2017-02-28 19:49:26 -0700

    Cleaned up

--- Untitled Folder/test.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'TimedMessage'
-length= ARGV[0].to_i;
-# puts ARGV[1];
-TimedMessage.timed_delayed_message(length,ARGV[1])
-# fork{
-#   sleep(5)
-#   puts "fucking heated"
-# }
-# puts "done"
